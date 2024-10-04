c73f2d4c440157ae26a5e306e7c88575f96e1f08
tursunait <tursunait05@gmail.com>
2024-10-04 13:49:23 -0400

    Load

--- mylib/transform_load.py
+++ mylib/transform_load.py
@@ -3,24 +3,29 @@ Transforms and Loads data into the local SQLite3 database
 Example:
 ,general name,count_products,ingred_FPro,avg_FPro_products,avg_distance_root,ingred_normalization_term,semantic_tree_name,semantic_tree_node
 """
+
 import sqlite3
 import csv
 import os
 
-#load the csv file and insert into a new sqlite3 database
-def load(dataset="/workspaces/sqlite-lab/data/GroceryDB_IgFPro.csv"):
-    """"Transforms and Loads data into the local SQLite3 database"""
 
-    #prints the full working directory and path
+# load the csv file and insert into a new sqlite3 database
+def load(
+    dataset="/Users/tusunaiturumbekova/Desktop/DE/py_script_with_SQLDatabase/data/urbanization.csv",
+):
+    """ "Transforms and Loads data into the local SQLite3 database"""
+
+    # prints the full working directory and path
     print(os.getcwd())
-    payload = csv.reader(open(dataset, newline=''), delimiter=',')
-    conn = sqlite3.connect('GroceryDB.db')
+    payload = csv.reader(open(dataset, newline=""), delimiter=",")
+    conn = sqlite3.connect("urbanizationDB.db")
     c = conn.cursor()
-    c.execute("DROP TABLE IF EXISTS GroceryDB")
-    c.execute("CREATE TABLE GroceryDB (id,general_name, count_products, ingred_FPro, avg_FPro_products, avg_distance_root, ingred_normalization_term, semantic_tree_name, semantic_tree_node)")
-    #insert
-    c.executemany("INSERT INTO GroceryDB VALUES (?,?, ?, ?, ?, ?, ?, ?, ?)", payload)
+    c.execute("DROP TABLE IF EXISTS urbanizationDB")
+    c.execute(
+        "CREATE TABLE urbanizationDB (statefips,state,gisjoin,lat_tract,long_tract,population,adj_radiuspop_5,urbanindex)"
+    )
+    # insert
+    c.executemany("INSERT INTO urbanizationDB VALUES (?,?, ?, ?, ?, ?, ?, ?)", payload)
     conn.commit()
     conn.close()
-    return "GroceryDB.db"
-
+    return "urbanizationDB"
