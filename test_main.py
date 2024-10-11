0fe850c5e9824d03b26ecfe6c75cdc4949240d3e
tursunait <tursunait05@gmail.com>
2024-10-11 10:56:41 -0700

    Test fixes

--- test_main.py
+++ test_main.py
@@ -1,52 +1,34 @@
 import unittest
 from unittest.mock import patch
-import main  # Only keep the main import for calling main.main()
+import main
 
 
 class TestMain(unittest.TestCase):
 
-    @patch("main.extract")  # Mock where extract is used, i.e., in `main.py`
-    @patch(
-        "sys.argv",
-        [
-            "main.py",
-            "extract",
-            "--url",
-            "http://example.com",
-            "--file_path",
-            "data/example.csv",
-        ],
-    )
+    @patch("main.extract")
+    @patch("sys.argv", ["main.py", "extract"])
     def test_extract(self, mock_extract):
         """Test the extract functionality."""
-        main.main()  # Call the main function to trigger the logic
-        mock_extract.assert_called_once_with("http://example.com", "data/example.csv")
+        main.main()
+        mock_extract.assert_called_once()
 
-    @patch("main.load")  # Mock where load is used, i.e., in `main.py`
-    @patch(
-        "sys.argv", ["main.py", "transform_load", "--dataset", "data/urbanization.csv"]
-    )
+    @patch("main.load")
+    @patch("sys.argv", ["main.py", "transform_load"])
     def test_transform_load(self, mock_load):
         """Test the transform and load functionality."""
-        main.main()  # Call the main function to trigger the logic
-        mock_load.assert_called_once_with("data/urbanization.csv")
+        main.main()
+        mock_load.assert_called_once()
 
-    @patch("main.general_query")  # Corrected the function name to general_query
+    @patch("main.general_query")
     @patch(
         "sys.argv",
-        [
-            "main.py",
-            "general_query",
-            """SELECT *
-            FROM default.urbanizationdb 
-            LIMIT 10""",
-        ],
+        ["main.py", "general_query", "SELECT * FROM default.urbanizationdb LIMIT 10"],
     )
     def test_general_query(self, mock_general_query):
         """Test the general query functionality."""
-        main.main()  # Call the main function to trigger the logic
+        main.main()
         mock_general_query.assert_called_once_with(
-            """SELECT * FROM default.urbanizationdb LIMIT 10"""
+            "SELECT * FROM default.urbanizationdb LIMIT 10"
         )
 
 
